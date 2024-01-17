89b32ad14a296ac21ee4c46a3e9853fb42298655
Angel Coronel <angelcoronel15@gmail.com>
2024-01-16 17:38:41 -0700

    Added prework files to repo

---
+++ CodePathPreWork/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  CodePathPreWork
+//
+//  Created by Angel Coronel on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    @IBOutlet weak var schoolLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    @IBAction func changeLabelsColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        let randomColor = changeColor()
+        nameLabel.textColor = randomColor
+        schoolLabel.textColor = randomColor
+        jobLabel.textColor = randomColor
+    }
+}
+
